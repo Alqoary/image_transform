9fd82f2464c7e40d52e35f30765bfd3150458ec2
alqoary <alquaaryabdalrhman@gmail.com>
2021-12-26 15:01:40 +0300

    image transform in C

---
+++ include/Trans.h
@@ -0,0 +1,12 @@
+#ifndef __TRANS__
+#define __TRANS__
+
+void print_image(float **image, int size);
+void translate(float **image, int size, int startX, int startY, int endX
+	, int endY, int	width, int height);
+float **create_matrix(int height, int width);
+float **extract_feature(float **image, int img_size, int X, int Y, int height, int width);
+
+
+
+#endif 
